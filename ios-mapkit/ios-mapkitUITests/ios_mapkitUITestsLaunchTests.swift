4af2ae6e951c08af2ba8adf32c91807ff1a4e8b0
Alex Quiton <80876135+aquiton@users.noreply.github.com>
2023-11-28 08:57:53 -0500

    init project files

---
+++ ios-mapkit/ios-mapkitUITests/ios_mapkitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ios_mapkitUITestsLaunchTests.swift
+//  ios-mapkitUITests
+//
+//  Created by Alex Quiton on 11/28/23.
+//
+
+import XCTest
+
+final class ios_mapkitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
